1b31e45ec5c9cac554a25b9a371b5073278ae770
singsai <nicktang@gmail.com>
2011-02-21 23:51:01 -0500

    Added a Users controller and layout link tests

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -7,19 +7,43 @@ describe "LayoutLinks" do
     response.should have_selector('title', :content => "Home")
   end
   
-  # it "should have a Contact page at '/contact' " do
-  #   get '/contact'
-  #   response.should have_selector('title', :content => "Contact")
-  # end
-  # 
-  # it "should have an About page at '/about'" do
-  #   get '/about'
-  #   response.should have_selector('title', :content => "About")    
-  # end
-  # 
-  # it "should have a Help page at '/help'" do
-  #   get '/help'
-  #   response.should have_selector('help', :content => "Help")
-  # end
+  it "should have a Contact page at '/contact' " do
+      get '/contact'
+      response.should have_selector('title', :content => "Contact")
+    end
+    
+    it "should have an About page at '/about'" do
+      get '/about'
+      response.should have_selector('title', :content => "About")    
+    end
+    
+    it "should have a Help page at '/help'" do
+      get '/help'
+      response.should have_selector('title', :content => "Help")
+    end
+
+    it "should have a Signup page at '/signup'" do
+      get '/signup'
+      response.should have_selector('title', :content => "Sign up")
+    end
+
+    it "should have the right links on the layout" do
+      visit root_path
+      response.should have_selector('title', :content => "Home")
+      
+      click_link "About"
+      response.should have_selector('title', :content => "About")
+      
+      click_link "Contact"
+      response.should have_selector('title', :content => "Contact")
+      
+      click_link "Home"
+      response.should have_selector('title', :content => "Home")
+      
+      click_link "Sign up now"
+      response.should have_selector('title', :content => "Sign up")
+      
+      response.should have_selector('a[href="/"]>img')
+    end
 
 end
