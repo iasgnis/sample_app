13359fd58824c55ea4b3383f799352d312546383
singsai <nicktang@gmail.com>
2011-02-21 22:24:23 -0500

    Added layout links (with routes)

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -54,4 +54,20 @@ render_views
     
   end
 
+  describe "GET 'help'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'help'
+      response.should have_selector("title", 
+                :content => "#{@base_title} | Help")
+    end
+    
+  end
+
+
+
 end
